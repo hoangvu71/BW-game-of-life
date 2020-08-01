99c59b246f8ba9e2aa7686c4dc12a405c77dfbb8
Devin <hoangong71@gmail.com>
2020-07-31 19:48:32 -0700

    Added toggle

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {speed: 0, start: true}
+    this.state = {speed: 0, start: true, random : false}
   }
 
   drawGrid(){
@@ -11,9 +11,9 @@ class App extends React.Component {
     const canvas = document.querySelector('canvas')
     const ctx = canvas.getContext('2d');
 
-    const resolution = 4
-    canvas.width = 400
-    canvas.height = 400
+    const resolution = 10
+    canvas.width = 800
+    canvas.height = 800
 
     const COLS = canvas.width / resolution
     const ROWS = canvas.height / resolution
@@ -33,6 +33,10 @@ class App extends React.Component {
     function update() {
       if (everything.state.start){
         grid = nextGen(grid);
+        if (everything.state.random){
+          grid = buildGrid()
+        }
+        everything.setState({random: false})
         renderGrid(grid)
         setTimeout(() => {
           requestAnimationFrame(update)
@@ -80,18 +84,25 @@ class App extends React.Component {
     return nextGen;
     }
 
-    function renderGrid(grid) {
-      for (let col = 0; col < grid.length; col++) {
-        for (let row = 0; row < grid[col].length; row++) {
+    function renderGrid() {
+      for (let col = 0; col < COLS; col++) {
+        for (let row = 0; row < ROWS; row++) {
           const cell = grid[col][row];
 
           ctx.beginPath();
           ctx.rect(col * resolution, row * resolution, resolution, resolution)
-          ctx.fillStyle = cell ? 'black' : 'white';
+          ctx.fillStyle = cell ? 'darkgray' : 'white';
           ctx.fill()
         }
       }
     }
+
+    canvas.addEventListener('click', function(event) {
+      var cellX = Math.floor((event.clientX) / resolution);
+      var cellY = Math.floor((event.clientY + resolution *2) / resolution);
+      grid[cellX][cellY] = !grid[cellX][cellY];
+      renderGrid();
+    });
 
     function slider_speed() {
     
@@ -112,16 +123,19 @@ class App extends React.Component {
 
   } 
 
-  startStop() {
-    this.setState({start: !this.state.start})
-    console.log(this.state)
+  startStop(event) {
+    console.log(event.target.value)
+    if (event.target.value === "start") {
+      this.setState({start: true})
+    }
+    else if (event.target.value === "stop") {
+      this.setState({start: false})
+    }
   }
   
   componentDidMount() {
     this.drawGrid()
   }
-
-
 
   render() {
     return (
@@ -129,11 +143,18 @@ class App extends React.Component {
           <h1>Hello CodeSandbox</h1>
           <h2>Start editing to see some magic happen!</h2>
           <canvas></canvas>
-          <button onClick={this.drawGrid}>Change</button>
+          <button onClick={() => {
+            this.setState({random: true})
+          }}>Change</button>
           <input type="range" min="1" max="500" class="slider" id="myRange"></input>
-          <p>Value: <span id="valueSlider"></span></p>
-          <button onClick={() => this.startStop()}>Start</button>
-          <button onClick={() => this.startStop()}>Stop</button>
+          <p>Speed: <span id="valueSlider"></span> milliseconds</p>
+          <button value = "start" onClick={(event) => this.startStop(event)}>Start</button>
+          <button value = "stop" onClick={(event) => this.startStop(event)}>Stop</button>
+          <div>The universe of the Game of Life is an infinite, two-dimensional orthogonal grid of square cells, each of which is in one of two possible states, live or dead, (or populated and unpopulated, respectively). Every cell interacts with its eight neighbours, which are the cells that are horizontally, vertically, or diagonally adjacent. At each step in time, the following transitions occur:
+                <div>Any live cell with fewer than two live neighbours dies, as if by underpopulation.</div>
+                <div>Any live cell with two or three live neighbours lives on to the next generation.</div>
+                <div>Any live cell with more than three live neighbours dies, as if by overpopulation.</div>
+                <div>Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.</div></div>
         </div>
       );
       }
