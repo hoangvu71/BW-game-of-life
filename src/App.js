51a1d034ae06e8552c0923bd514471e4547ca939
Devin <hoangong71@gmail.com>
2020-12-10 17:23:17 -0800

    Another == to === line 137 app.js

--- src/App.js
+++ src/App.js
@@ -134,7 +134,7 @@ class App extends React.Component {
           else if (cell === 1 && numNeighbors > 3) {
             nextGen[col][row] = 0;
           }
-          else if (cell === 0 && numNeighbors == 3) {
+          else if (cell === 0 && numNeighbors === 3) {
             nextGen[col][row] = 1;
           }
           
